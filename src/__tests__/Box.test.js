4a750d1b9431091b8cfac6e3914c82977326733f
Muhammad Irsyad <irsyad@warungpintar.co>
2022-03-01 12:11:41 +0700

    feat: add box props unit test

--- src/__tests__/Box.test.js
+++ src/__tests__/Box.test.js
@@ -5,18 +5,36 @@ import {
 } from 'native-base';
 
 describe('<Box />', () => {
-  const { queryByTestId } = render(
-    <NativeBaseProvider
-      initialWindowMetrics={{
-        frame: { x: 0, y: 0, width: 0, height: 0 },
-        insets: { top: 0, left: 0, right: 0, bottom: 0 },
-      }}
-    >
-      <Box testID="my-box">Test Box</Box>
-    </NativeBaseProvider>
-  )
   it('should render box component properly', () => {
+    const { queryByTestId } = render(
+      <NativeBaseProvider
+        initialWindowMetrics={{
+          frame: { x: 0, y: 0, width: 0, height: 0 },
+          insets: { top: 0, left: 0, right: 0, bottom: 0 },
+        }}
+      >
+        <Box testID="my-box">Test Box</Box>
+      </NativeBaseProvider>
+    )
+
     expect(queryByTestId('my-box')).not.toBeNull()
+  });
+
+  it('should render box props properly', () => {
+    const { queryByTestId } = render(
+      <NativeBaseProvider
+        initialWindowMetrics={{
+          frame: { x: 0, y: 0, width: 0, height: 0 },
+          insets: { top: 0, left: 0, right: 0, bottom: 0 },
+        }}
+      >
+        <Box testID="my-box" backgroundColor="white" borderColor="black" marginBottom={4}>Test Box</Box>
+      </NativeBaseProvider>
+    )
+    
+    expect(queryByTestId('my-box').props.style.backgroundColor).toMatch('#FFFFFF');
+    expect(queryByTestId('my-box').props.style.borderColor).toMatch('#000000');
+    expect(queryByTestId('my-box').props.style.marginBottom).toEqual(16);
   });
 
 })
