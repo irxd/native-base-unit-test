49e1c647ce7945fb6ba99952d2981d20a5c22592
Muhammad Irsyad <irsyad@warungpintar.co>
2022-03-01 15:36:33 +0700

    feat: add image props unit test

--- src/__tests__/Image.test.js
+++ src/__tests__/Image.test.js
@@ -5,23 +5,46 @@ import {
 } from 'native-base';
 
 describe('<Image />', () => {
-  const { queryByTestId } = render(
-    <NativeBaseProvider
-      initialWindowMetrics={{
-        frame: { x: 0, y: 0, width: 0, height: 0 },
-        insets: { top: 0, left: 0, right: 0, bottom: 0 },
-      }}
-    >
-      <Image
-        source={{ uri: "https://wallpaperaccess.com/full/317501.jpg"}}
-        alt="Alternate Text"
-        size="xl"
-        testID="my-image"
-      />
-    </NativeBaseProvider>
-  )
   it('should render image component properly', () => {
+    const { queryByTestId } = render(
+      <NativeBaseProvider
+        initialWindowMetrics={{
+          frame: { x: 0, y: 0, width: 0, height: 0 },
+          insets: { top: 0, left: 0, right: 0, bottom: 0 },
+        }}
+      >
+        <Image
+          source={{ uri: "https://wallpaperaccess.com/full/317501.jpg"}}
+          alt="Alternate Text"
+          size="xl"
+          testID="my-image"
+        />
+      </NativeBaseProvider>
+    );
+
     expect(queryByTestId('my-image')).not.toBeNull()
   });
 
-})
+  it('should render image props properly', () => {
+    const { queryByTestId } = render(
+      <NativeBaseProvider
+        initialWindowMetrics={{
+          frame: { x: 0, y: 0, width: 0, height: 0 },
+          insets: { top: 0, left: 0, right: 0, bottom: 0 },
+        }}
+      >
+        <Image
+          source={{ uri: "https://wallpaperaccess.com/full/317501.jpg"}}
+          alt="Alternate Text"
+          width={500}
+          height={500}
+          testID="my-image"
+        />
+      </NativeBaseProvider>
+    );
+    
+    expect(queryByTestId('my-image').props.source.uri).toMatch("https://wallpaperaccess.com/full/317501.jpg");
+    expect(queryByTestId('my-image').props.style.width).toEqual(500);
+    expect(queryByTestId('my-image').props.style.height).toEqual(500);
+  });
+});
