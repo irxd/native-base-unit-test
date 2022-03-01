1f870ed7c0c83276b4dd7595edc6f2c012df5bc8
Muhammad Irsyad <irsyad@warungpintar.co>
2022-03-01 11:23:20 +0700

    feat: add text props unit test

--- src/__tests__/Text.test.js
+++ src/__tests__/Text.test.js
@@ -5,18 +5,34 @@ import {
 } from 'native-base';
 
 describe('<Text />', () => {
-  const { queryByTestId } = render(
-    <NativeBaseProvider
-      initialWindowMetrics={{
-        frame: { x: 0, y: 0, width: 0, height: 0 },
-        insets: { top: 0, left: 0, right: 0, bottom: 0 },
-      }}
-    >
-      <Text testID="my-text">hello world</Text>
-    </NativeBaseProvider>
-  )
   it('should render text component properly', () => {
+    const { queryByTestId } = render(
+      <NativeBaseProvider
+        initialWindowMetrics={{
+          frame: { x: 0, y: 0, width: 0, height: 0 },
+          insets: { top: 0, left: 0, right: 0, bottom: 0 },
+        }}
+      >
+        <Text testID="my-text">hello world</Text>
+      </NativeBaseProvider>
+    )
+
     expect(queryByTestId('my-text')).not.toBeNull()
   });
 
+  it('should render text props properly', () => {
+    const { queryByTestId } = render(
+      <NativeBaseProvider
+        initialWindowMetrics={{
+          frame: { x: 0, y: 0, width: 0, height: 0 },
+          insets: { top: 0, left: 0, right: 0, bottom: 0 },
+        }}
+      >
+        <Text testID="my-text" fontWeight="bold" color="white">hello world</Text>
+      </NativeBaseProvider>
+    )
+    
+    expect(queryByTestId('my-text').props.style.fontWeight).toMatch('700');
+    expect(queryByTestId('my-text').props.style.color).toMatch('#FFFFFF');
+  });
 })
